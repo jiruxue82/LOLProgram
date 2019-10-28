0ac4e956062a30e3a93689091d56004f4afaf693
姬如雪 <jiruxue82@gmail.com>
2019-10-28 22:03:45 +0800

    hello world

---
+++ ChinaFightJapan/Classes/HelloWorld.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld.swift
+//  Howword
+//
+//  Created by mac on 2019/10/28.
+//  Copyright © 2019 bingdaohuoshan. All rights reserved.
+//
+
+import UIKit
+import Speech
+class HelloWorld: NSObject {
+
+    public class func sayHelloWorld() {
+        print("hello----world")
+    }
+    
+}
---
+++ Example/Pods/Target Support Files/Pods-ChinaFightJapan_Example/Pods-ChinaFightJapan_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChinaFightJapan_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ChinaFightJapan_Example
+@end
