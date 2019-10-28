d5183ed43d0f857aec62d19a563b09da93885378
姬如雪 <jiruxue82@gmail.com>
2019-10-28 17:22:50 +0800

    提交代码

---
+++ Example/Pods/Target Support Files/LOLProgram/LOLProgram-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LOLProgram : NSObject
+@end
+@implementation PodsDummy_LOLProgram
+@end
---
+++ Example/Pods/Target Support Files/Pods-LOLProgram_Example/Pods-LOLProgram_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LOLProgram_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LOLProgram_Example
+@end
